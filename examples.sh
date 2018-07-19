e86bcc6b28a9d2e152d1e5aafd1ffe47f1dd46f6
Freecks <leo.fourrier@hotmail.fr>
2018-07-19 11:40:41 +0200

    remove useless

--- examples.sh
+++ examples.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+hdfs dfs -mkdir -p input
+hdfs dfs -put /home/lifoadm/Documents/data/retail.dat input/
+#hdfs dfs -put /home/lifoadm/Documents/data/webdocs.dat input/
+hdfs dfs -rmr output
+
+#hdfs dfs -put ../data/webdocs.dat input/webdocs.dat
+
+
 ##examples are for use with webdocs, arguments are input, output, support, number of results to display and number of reducers
 
-#support = 30%
-hadoop jar FrequentItemsets.jar App input/webdocs.dat output 507625 10 32
+#Webdoc / support = 30% ~ 507625 
+#hadoop jar  target/leo-0.0.1-SNAPSHOT.jar  stage.leo.App input/webdocs.dat output 507625 10 1
+
+#retail.dat / support:1000 
+hadoop jar  target/leo-0.0.1-SNAPSHOT.jar  stage.leo.App input/retail.dat output 1000 10 2
+#hadoop jar  target/leo-0.0.1-SNAPSHOT.jar  stage.leo.App input/retail.dat output 300 8 8
+
 
 #support = 29%
 #hadoop jar FrequentItemsets.jar App input/webdocs.dat output 490704 10 32
@@ -15,4 +28,4 @@ hadoop jar FrequentItemsets.jar App input/webdocs.dat output 507625 10 32
 #hadoop jar FrequentItemsets.jar App input/webdocs.dat output 456862 10 32
 
 #support = 26%
-#hadoop jar FrequentItemsets.jar App input/webdocs.dat output 439941 10 32
+#hadoop jar target/leo-0.0.1-SNAPSHOT.jar stage.leo.App input/webdocs.dat output 439941 10 2
